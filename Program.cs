d0f2fa5ad999d58395b8fc6cf35fc72e21596760
Connor McRoberts <connor915218@gmail.com>
2025-04-10 10:22:49 -0400

    added static data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿
-namespace Inclass
+﻿using System.Text.Json.Nodes;
+
+
+
+namespace InclassActivity {
+
+    private class employee() {
+
+        string department;
+        string name;
+        int salary;
+
+        public employee(string department, string name, int salary) {
+            this.department = department;
+            this.name = name;
+            this.salary = salary;
+        }
+    }
+ 
+    static class Main() {
+
+        var data = [new employee("HR", "Alice", 50000), new employee("IT", "Bob", 70000), 
+        new employee("HR", "Charlie", 60000), new employee("Marketing", "Eve", 55000), 
+        new employee("Marketing", "Frank", 58000)];
+
+        
+
+    }
+}
